ad886c8e0471c9e6d88e75d01fae4e1bf1e08f85
wuhaisheng <15001167619@163.com>
2018-11-09 17:55:00 +0800

    去除不要的类

--- api-gateway/src/main/java/com/kmc/gateway/filter/AuthFilter.java
+++ api-gateway/src/main/java/com/kmc/gateway/filter/AuthFilter.java
@@ -2,7 +2,6 @@ package com.kmc.gateway.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import com.netflix.zuul.exception.ZuulException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 
